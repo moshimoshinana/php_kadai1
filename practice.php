8da8ea51a675366c6a4774a72d1b544e15fc26c3
moshimoshinana <74526386+moshimoshinana@users.noreply.github.com>
2020-11-20 06:11:18 +0900

    Update practice.php

--- practice.php
+++ practice.php
@@ -1,24 +1,42 @@
 <?php
 echo 'hello php';
+echo "\n";
 
 $a = 3;
 $b = 7;
 echo $a + $b;
+echo "\n";
 
 $array_month = ["1月","2月","3月","4月","5月","6月",
 "7月","8月","9月","10月","11月","12月"];
-echo $array_month[7];
+$array_month[7];
+echo "\n";
 
 $hello = "Hello,";
 $name = "Nana";
 $world = "'s World";
 echo $hello . $name . $world;
+echo "\n";
 
-$tech_boost = "tech ";
+$tech_boost = "tech";
 $tech_boost .= "boost";
 echo $tech_boost;
+echo "\n";
 
-//文字列に数字が使えない
-// 10月にダブルクォーテーションがない
-// 配列の時{}ではなく[]を使う
-// ehoのあとの[December]にダブルクォーテーションがない
+
+$calendar_2018 = [
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月"
+];
+echo $calendar_2018["December"];
+
